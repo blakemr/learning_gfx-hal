e9c49877d8594b903981a12bedb894a3c26f206b
Matthew Blake <mblak0625@gmail.com>
2019-10-08 18:33:44 -0400

    Finished ::new and ::default for EventWindow, and added consts for window title and size.

--- src/bin/gfx.rs
+++ src/bin/gfx.rs
@@ -2,9 +2,19 @@
 //! attempt 2
 //! Step 1: Fully understand how to make a window
 //! this part shouldn't require gfx-hal at all
-use winit::{Event, EventsLoop, WindowBuilder, WindowEvent};
+use winit::dpi::LogicalSize;
+use winit::{CreationError, Event, EventsLoop, Window, WindowBuilder, WindowEvent};
 
-/// Window struct. Comprised of a window and an event loop
+/// CONSTANTS
+// Window name
+pub const WINDOW_TITLE: &str = "I'm Learning";
+// Window size
+pub const WINDOW_SIZE: LogicalSize = LogicalSize {
+    width: 500.0,
+    height: 500.0,
+};
+
+/// Window with an event loop
 ///
 /// # args
 /// * **events_loop** *EventsLoop* - event loop object. Controls events. duh
@@ -12,27 +22,59 @@ use winit::{Event, EventsLoop, WindowBuilder, WindowEvent};
 #[derive(Debug)]
 pub struct EventWindow {
     pub events_loop: EventsLoop,
-    pub window: WindowBuilder,
+    pub window: Window,
+}
+
+impl EventWindow {
+    /// Init new EventWindow with events_loop and window
+    ///
+    /// # args
+    /// * **title** *String or reference* - Window title
+    /// * **size** *LogicalSize* - Window size (x, y).
+    ///
+    /// # returns
+    /// * Self or CreationError
+    ///
+    /// # errors
+    /// * **CreationError** - winit error that can happen when trying to make a window
+    ///
+    pub fn new<T: Into<String>>(title: T, size: LogicalSize) -> Result<Self, CreationError> {
+        // Init events loop and window
+        let events_loop = EventsLoop::new();
+        let window = WindowBuilder::new()
+            .with_title(title)
+            .with_dimensions(size)
+            .build(&events_loop);
+
+        // window is currently a result type, so that needs to be mapped to Self
+        // iff it's not an error.
+        window.map(|window| Self {
+            events_loop,
+            window,
+        })
+    }
+}
+
+impl Default for EventWindow {
+    /// Default window. Uses the WINDOW_TITLE and WINDOW_SIZE consts
+    ///
+    /// # Errors
+    /// * CreationError - Error when building window
+    fn default() -> Self {
+        Self::new(WINDOW_TITLE, WINDOW_SIZE).expect("Window creation failed: EventWindow.default()")
+    }
 }
 
 /// Main function. Main loop goes here
 fn main() {
-    // Winit needs an events loop, so lets give it one.
-    // Note: poll_events requires this to be mutable,
-    // but the docs don't really cover that. FYI
-    let mut events_loop = EventsLoop::new();
-
-    // Init the window with a builder for later customization
-    let builder = WindowBuilder::new()
-        .with_title("Init a window")
-        .build(&events_loop)
-        .expect("Could not build window.");
+    // Create a new EventWindow
+    let mut event_window = EventWindow::default();
 
     // Main loop
     let mut exit = false;
     while !exit {
         // Every loop, poll events for input
-        events_loop.poll_events(|event| match event {
+        event_window.events_loop.poll_events(|event| match event {
             // Check for an exit, if so break loop and end
             Event::WindowEvent {
                 event: WindowEvent::CloseRequested,
